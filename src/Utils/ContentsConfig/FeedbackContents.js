2ba9ad97287d8682026c3c0f42b2b4ea139a3f54
Danish Zayan <danishzayan6@gmail.com>
2024-11-14 12:17:46 +0530

    feat: Add new content configurations for alert messages, context menus, dialogs, loaders, notifications, skeletons, testimonials, and tree dropdowns

---
+++ src/Utils/ContentsConfig/FeedbackContents.js
@@ -0,0 +1,171 @@
+// Alert Message contents
+export const alertMessageContents = [
+    {
+        id: 1,
+        title: 'Alert Message With Background',
+        href: '#alert_message_with_background',
+    },
+    {
+        id: 2,
+        title: 'Alert Message With Title',
+        href: '#alert_message_with_title',
+    },
+    {
+        id: 3,
+        title: 'Alert Message With Border',
+        href: '#alert_message_with_border',
+    },
+    {
+        id: 4,
+        title: 'Message Take Action',
+        href: '#message_take_action',
+    },
+];
+
+// Context Menu contents
+export const contextMenuContents = [
+    {
+        id: 1,
+        title: 'Context Menu',
+        href: '#context_menu',
+    },
+    {
+        id: 2,
+        title: 'Context Menu With Dropdown',
+        href: '#context_menu_with_dropdown',
+    },
+];
+
+// Dialog contents
+export const dialogContents = [
+    {
+        id: 1,
+        title: 'Basic Dialog',
+        href: '#basic_dialog',
+    },
+];
+
+// Loader contents
+export const loaderContents = [
+    {
+        id: 1,
+        title: 'Circle Loader',
+        href: '#circle_loader',
+    },
+    {
+        id: 2,
+        title: 'Dashed Loader',
+        href: '#dashed_loader',
+    },
+    {
+        id: 3,
+        title: 'Opacity Loader',
+        href: '#opacity_loader',
+    },
+    {
+        id: 4,
+        title: 'Wave Loader',
+        href: '#wave_loader',
+    },
+    {
+        id: 5,
+        title: 'Chase Loader',
+        href: '#chase_loader',
+    },
+    {
+        id: 6,
+        title: 'Dot Loader',
+        href: '#dot_loader',
+    },
+    {
+        id: 7,
+        title: 'Shape Loader',
+        href: '#shape_loader',
+    },
+    {
+        id: 8,
+        title: 'Flip Loader',
+        href: '#flip_loader',
+    },
+];
+
+// Notification contents
+export const notificationContents = [
+    {
+        id: 1,
+        title: 'Border Notification',
+        href: '#border_notification',
+    },
+    {
+        id: 2,
+        title: 'Icon Notification',
+        href: '#icon_notification',
+    },
+    {
+        id: 3,
+        title: 'Custom Position Notification',
+        href: '#custom_position_notification',
+    },
+    {
+        id: 4,
+        title: 'Push Notification',
+        href: '#push_notification',
+    },
+    {
+        id: 5,
+        title: 'Progress Notification',
+        href: '#progress_notification',
+    },
+];
+
+// Skeleton contents
+export const skeletonContents = [
+    {
+        id: 1,
+        title: 'Card Skeleton',
+        href: '#card_skeleton',
+    },
+    {
+        id: 2,
+        title: 'Image Gallery Skeleton',
+        href: '#image_gallery_skeleton',
+    },
+    {
+        id: 3,
+        title: 'Social Post Skeleton',
+        href: '#social_post_skeleton',
+    },
+    {
+        id: 4,
+        title: 'Product Details Skeleton',
+        href: '#product_details_skeleton',
+    },
+    {
+        id: 5,
+        title: 'Post List Skeleton',
+        href: '#post_list_skeleton',
+    },
+    {
+        id: 6,
+        title: 'Blog Post Skeleton',
+        href: '#blog_post_skeleton',
+    },
+];
+
+// Testimonial contents
+export const testimonialContents = [
+    {
+        id: 1,
+        title: 'Testimonial',
+        href: '#testimonial',
+    },
+];
+
+// Tree Dropdown contents
+export const treeDropdownContents = [
+    {
+        id: 1,
+        title: 'Tree Dropdown',
+        href: '#tree_dropdown',
+    },
+];
