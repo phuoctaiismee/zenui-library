de14fc93629fc57d556c13e13bbadfeb86c37c99
asfak00 <asfakahmed680@gmail.com>
2024-11-16 21:32:31 +0600

    update: newsboard updated

--- src/Components/Home/NewsBoard.jsx
+++ src/Components/Home/NewsBoard.jsx
@@ -4,13 +4,13 @@ const NewsBoard = () => {
     return (
         <div
             className='relative overflow-hidden w-full bg-gradient-to-r brightness-100 hover:brightness-125 to-primary from-[#9A04F5] h-[50px] flex items-center justify-center'>
-            <img alt='image' src='https://i.ibb.co.com/Ns7HHmB/confetti.png'
-                 className='absolute top-6 640px:top-0 left-0 w-[50px] 640px:w-[90px] opacity-80'/>
+            <img alt='image' src='https://i.ibb.co.com/qNvQzcm/image-317.png'
+                 className='absolute top-3 640px:top-[-5px] 640px:left-1 left-0 w-[50px] 640px:w-[100px]'/>
 
-            <p className='text-white text-[0.9rem] 640px:text-[1rem] font-[400]'>E-Commerce Components & Blocks Coming 🚀</p>
+            <p className='text-white text-[0.9rem] 640px:text-[1rem] text-center font-[400]'><b>ZenUI 2.2</b> Coming With E-Commerce Solution 🚀</p>
 
-            <img alt='image' src='https://i.ibb.co.com/Ns7HHmB/confetti.png'
-                 className='absolute top-6 640px:top-0 right-0 w-[50px] 640px:w-[90px] opacity-80 rotate-[260deg]'/>
+            <img alt='image' src='https://i.ibb.co.com/0MVyTcX/image-318.png'
+                 className='absolute top-2 640px:top-[-20px] right-0 w-[100px] 640px:w-[160px]'/>
         </div>
     );
 };
